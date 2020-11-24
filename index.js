0fc09f2e76da8a36c3eb3d740fec20a83c731cfa
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-11-24 01:16:28 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  const winner = arr.find (game =>{
+    return game.result === "W"
+  })
+  return winner? winner.year : winner
+}
+  
